c5385c904ff1b37cedab16415889826508b1eab4
Christian <150170670+ThirdRevelation7@users.noreply.github.com>
2023-12-21 18:25:37 -0500

    Major update Added multiple new features and ui improvements.

--- blackjack.py
+++ blackjack.py
@@ -4,42 +4,65 @@ from art import logo
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 game_still_going = True
 
-def check_for_blackjack(dealer_hand, user_hand):
-    if sum(dealer_hand) == 21:
-        print("The dealer got Blackjack, the dealer wins.")
-        
+def check_for_blackjack(user_hand, dealer_hand):
+    if sum(user_hand) == 21 and sum(dealer_hand) == 21:
+        return False
     elif sum(user_hand) == 21:
-        print("You got Blackjack, you win!")
-        
-    elif sum(dealer_hand) == 21 and sum(user_hand) == 21:
-        print("You both got Blackjack, dealer wins. ")
-        
+        return True
+    elif sum(dealer_hand) == 21:
+        return False
 
+def ace_value(hand):
+    for i in range(len(hand)):
+        if hand[i] == 11:
+            hand[i] = 1
+           
 while game_still_going:
-    dealer_hand = random.sample(cards,3)
-    user_hand = random.sample(cards,3)
+    print(logo)
+    dealer_hand = []
+    while sum(dealer_hand) < 16:
+        dealer_hand += random.sample(cards, 1)  
+    user_hand = random.sample(cards, 2)
     dealer_score = 0
     user_score = 0
     dealer_score += sum(dealer_hand)
     user_score += sum(user_hand)
+    
+    print(f"Your cards: {user_hand}, Current Score: {user_score}")  
+    print(f"Dealers first card: {dealer_hand[0]}")
+    if check_for_blackjack(user_hand, dealer_hand) == True:
+        print("You got blackjack, you win!")
+        game_still_going = False
+    elif check_for_blackjack(user_hand, dealer_hand) == False:
+        print(f"Dealers Hand: {dealer_hand}")
+        print("The Dealer got Blackjack, the Dealer wins. Sorry.")
+        game_still_going = False
+    else:
+        another_card = input("Type 'y' to get another card, type 'p' to pass: ").lower()
+        if another_card == 'y':
+            user_hand += random.sample(cards, 1)
+            new_score = sum(user_hand)
+            if sum(user_hand) > 21:
+                ace_value(user_hand)
+                print(f"Your cardz: {user_hand}, Current Scorez: {sum(user_hand)}")
+            else:
+                print(f"Your cardz: {user_hand}, Current Scorez: {sum(user_hand)}")
+            
+        elif another_card == 'p' and dealer_score < 16:
+            dealer_hand += random.sample(cards, 1)
 
-    if user_score > 21:
-        for i in range(len(user_hand)):
-            if user_hand[i] == 11:
-                user_hand[i] = 1
-                user_score += sum(user_hand)
+        
+        print(dealer_hand)
+
 
             
-        print("went over 21")
+        
     
-    check_for_blackjack(dealer_hand, user_hand)
-    print(f"initial user hand: {user_hand}")
+    
+    
     
 
-    print(f"""
-        Dealer Hand: {dealer_hand}  Dealer Score: {dealer_score}
-        Player Hand: {user_hand}    Player Score: {user_score}
-""")
+
     
         
     break
